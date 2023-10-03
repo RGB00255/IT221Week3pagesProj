89197dc028fab4f85e24d00432ab010ca50fd927
RGB00255 <ryan.bergeron1998@gmail.com>
2023-10-03 16:43:09 -0400

    Class-based views

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+# pages/views.py
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "home.html"
