2313d6a027cb2c5e2386ad8537f15bdc60edd092
RGB00255 <ryan.bergeron1998@gmail.com>
2023-10-03 16:53:50 -0400

    Ready for deployment

--- pages_proj/settings.py
+++ pages_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3)(7mslw$fvw=crz0_24pf096@4tx^!r1e7c-v@0-mw8e7t8vn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
